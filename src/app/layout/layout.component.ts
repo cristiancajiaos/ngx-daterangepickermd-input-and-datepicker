fb5977305253dc40946938fee6b088fd87e60fc9
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-10-26 16:35:37 -0300

    Definido layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-layout',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  selected: {
+    startDate: Moment,
+    endDate: Moment
+  };
   constructor() { }
 
   ngOnInit() {
