09b6efee6b22b403d54a4892268e0b41c29d0c23
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-10-26 13:27:26 -0300

    Instaladas dependencias, establecido layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxDaterangepickerMd.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
